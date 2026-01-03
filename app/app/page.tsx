c925773038be6d6f2f5737947bb77e29dbfb4253
Hiroaki Hirai <hiro.hira.dev@gmail.com>
2026-01-04 07:11:10 +0900

    Fix Leaflet SSR issue by disabling SSR

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,8 @@
-import MapView from "@/components/MapView";
+import dynamic from "next/dynamic";
+
+const MapView = dynamic(() => import("@/components/MapView"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <MapView />;
